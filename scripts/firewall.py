b3fe1d6fe569d9474fdeb19a53c32c524650cea3
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 18:01:37 +0300

    .

--- scripts/firewall.py
+++ scripts/firewall.py
@@ -1,17 +1,31 @@
 import subprocess
 
 def improve_firewall_rules():
+    """
+    Improve UFW firewall rules to enhance system security.
+    """
     print("Improving firewall rules...")
-    # allow ssh connections from trusted IP addresses
-    ssh_ips = ["192.168.0.1", "192.168.0.2"]
-    for ip in ssh_ips:
-        cmd = f"sudo ufw allow from {ip} to any port 22"
-        subprocess.run(cmd, shell=True)
-    # allow HTTP and HTTPS traffic
-    cmd = "sudo ufw allow http"
-    subprocess.run(cmd, shell=True)
-    cmd = "sudo ufw allow https"
-    subprocess.run(cmd, shell=True)
-    # enable UFW firewall
-    cmd = "sudo ufw enable"
-    subprocess.run(cmd, shell=True)
+    subprocess.run(["sudo", "ufw", "default", "deny", "incoming"])
+    subprocess.run(["sudo", "ufw", "default", "allow", "outgoing"])
+    subprocess.run(["sudo", "ufw", "allow", "ssh"])
+    subprocess.run(["sudo", "ufw", "allow", "http"])
+    subprocess.run(["sudo", "ufw", "allow", "https"])
+    subprocess.run(["sudo", "ufw", "allow", "53"])
+    subprocess.run(["sudo", "ufw", "allow", "123/udp"])
+    subprocess.run(["sudo", "ufw", "allow", "5060/udp"])
+    subprocess.run(["sudo", "ufw", "allow", "5060/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "5190/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "5222/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "5223/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "5280/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "8443/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "8080/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "5672/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "27017/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "6379/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "9418/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "9418/udp"])
+    subprocess.run(["sudo", "ufw", "allow", "33434:33523/udp"])
+    subprocess.run(["sudo", "ufw", "allow", "33434:33523/tcp"])
+    subprocess.run(["sudo", "ufw", "allow", "speedtest.net"])
+    print("Firewall rules updated.")
