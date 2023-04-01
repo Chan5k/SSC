24cb3b5b8817bae3869195aeac7a12e51a19033e
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 23:12:02 +0300

    Update security.py

--- scripts/security.py
+++ scripts/security.py
@@ -1,7 +1,20 @@
+import os
 import subprocess
 
 def test_security():
-    print("Running security test...")
-    cmd = "sudo nmap -sS -sV --script vuln localhost"
-    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-    print(result.stdout)
+    """
+    Runs a security check on the system to detect malware and other security issues.
+    """
+    # Check for malware
+    print("Malware scan started, please wait")
+    output = subprocess.check_output(["sudo", "clamscan", "-r", "/"])
+
+    # Display the results
+    print(output.decode("utf-8"))
+
+    # Check for open ports
+    print("Checking open ports")
+    output = subprocess.check_output(["sudo", "netstat", "-tulpn"])
+
+    # Display the results
+    print(output.decode("utf-8"))
