136aa56111737c5a1bef408d04de85ce89a271e9
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 15:09:45 +0300

    .

--- scripts/cpu_stress_test.py
+++ scripts/cpu_stress_test.py
@@ -4,7 +4,11 @@ import time
 def test_cpu_stress(duration):
     print("Running CPU stress test for {} seconds...".format(duration))
     start_time = time.time()
+    cpu_usage_list = []
     while True:
         if time.time() - start_time >= duration:
             break
-        psutil.cpu_percent()
+        cpu_usage = psutil.cpu_percent()
+        cpu_usage_list.append(cpu_usage)
+    cpu_score = 100 - sum(cpu_usage_list) / len(cpu_usage_list)
+    print("CPU score: {:.2f}".format(cpu_score))
