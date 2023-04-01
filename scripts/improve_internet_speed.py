3f727f634ec5ed862cd8b1cd114e2237c80c78e9
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 18:50:50 +0300

    Update improve_internet_speed.py

--- scripts/improve_internet_speed.py
+++ scripts/improve_internet_speed.py
@@ -1,50 +1,16 @@
-import os
-import sys
 import subprocess
 
-def improve_internet_speed(interface):
-    print("This will make changes to your system's network settings. Do you wish to continue? (y/n)")
-    confirmation = input("> ")
-
-    if confirmation.lower() != "y":
-        print("Aborting.")
-        sys.exit()
-
-    print("Improving internet speed...")
-
-    # Disable IPv6
-    subprocess.run(["sudo", "sysctl", "-w", "net.ipv6.conf.all.disable_ipv6=1"])
-    subprocess.run(["sudo", "sysctl", "-w", "net.ipv6.conf.default.disable_ipv6=1"])
-
-    # Enable TCP Fast Open
-    subprocess.run(["sudo", "sysctl", "-w", "net.ipv4.tcp_fastopen=3"])
-
-    # Enable TCP congestion control BBR
-    subprocess.run(["sudo", "modprobe", "tcp_bbr"])
-    subprocess.run(["sudo", "sysctl", "-w", "net.core.default_qdisc=fq"])
-    subprocess.run(["sudo", "sysctl", "-w", "net.ipv4.tcp_congestion_control=bbr"])
-
-    # Prompt the user to choose a DNS server
-    print("Which DNS server do you want to use? Enter the corresponding number:")
-    print("1. Google DNS (8.8.8.8, 8.8.4.4)")
-    print("2. Cloudflare DNS (1.1.1.1, 1.0.0.1)")
-    dns_selection = input("> ")
-
-    # Set DNS servers based on user's choice
-    if dns_selection == "1":
-        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "8.8.8.8", "--interface", interface])
-        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "8.8.4.4", "--interface", interface])
-        print("DNS servers set to Google DNS (8.8.8.8, 8.8.4.4)")
-    elif dns_selection == "2":
-        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "1.1.1.1", "--interface", interface])
-        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "1.0.0.1", "--interface", interface])
-        print("DNS servers set to Cloudflare DNS (1.1.1.1, 1.0.0.1)")
+def improve_internet_speed():
+    print("This feature will change your DNS servers to Cloudflare or Google DNS servers.")
+    print("Please choose an option:")
+    print("1. Cloudflare")
+    print("2. Google")
+    choice = input("> ")
+    if choice == "1":
+        subprocess.run(["sudo", "sed", "-i", "s/nameserver.*/nameserver 1.1.1.2\nnameserver 1.0.0.2/g", "/etc/resolv.conf"])
+        print("DNS servers updated to Cloudflare.")
+    elif choice == "2":
+        subprocess.run(["sudo", "sed", "-i", "s/nameserver.*/nameserver 8.8.8.8\nnameserver 8.8.4.4/g", "/etc/resolv.conf"])
+        print("DNS servers updated to Google.")
     else:
         print("Invalid selection.")
-        sys.exit(1)
-
-    print("Done.")
-
-if __name__ == "__main__":
-    interface = input("Enter the name of your network interface: ")
-    improve_internet_speed(interface)
