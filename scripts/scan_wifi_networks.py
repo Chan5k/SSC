6aa1204f87e7a878de74b9812935e6029a833e82
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 20:54:44 +0300

    Update scan_wifi_networks.py

--- scripts/scan_wifi_networks.py
+++ scripts/scan_wifi_networks.py
@@ -1,45 +1,24 @@
 import subprocess
 
 def scan_wifi_networks():
-    """
-    Scans for available WiFi networks and returns a list of networks with their details.
-    """
-    # Run the iwlist command to scan for available WiFi networks
-    cmd_output = subprocess.check_output(['iwlist', 'wlan0', 'scan'], universal_newlines=True)
+    # run nmap to scan for wifi networks
+    cmd_output = subprocess.check_output(['sudo', 'nmap', '-sP', '-oG', '-','192.168.1.1/24'], universal_newlines=True)
 
-    # Split the output into a list of lines
-    lines = cmd_output.split('\n')
-
-    # Create a list to hold the available networks
+    # parse the output of nmap and extract the wifi networks
     networks = []
+    for line in cmd_output.split('\n'):
+        if 'Host:' in line:
+            fields = line.strip().split()
+            ip_address = fields[1]
+            hostname = fields[2].strip('()')
+            if hostname.startswith('WLAN_'):
+                networks.append({'ssid': hostname[5:], 'ip_address': ip_address})
 
-    # Loop through the lines and extract the network details
-    for line in lines:
-        if 'ESSID' in line:
-            ssid = line.split(':')[1].strip().strip('"')
-        elif 'Quality' in line:
-            quality = line.split('=')[1].split()[0]
-        elif 'Encryption key' in line:
-            encryption = line.split(':')[1].strip()
-        elif 'Frequency:' in line:
-            freq = line.split()[3].split(':')[1]
-        elif 'Mode:' in line:
-            mode = line.split(':')[1].strip()
-        elif 'Address:' in line:
-            address = line.split()[4]
-        elif 'Channel' in line:
-            channel = line.split(':')[1]
-
-            # Append the network to the list
-            networks.append({
-                'ssid': ssid,
-                'quality': quality,
-                'encryption': encryption,
-                'freq': freq,
-                'mode': mode,
-                'address': address,
-                'channel': channel
-            })
-
-    # Return the list of available networks
-    return networks
+    # print the available wifi networks
+    if len(networks) == 0:
+        print('No wifi networks found.')
+    else:
+        print('{:<20} {}'.format('SSID', 'IP Address'))
+        print('-' * 30)
+        for network in networks:
+            print('{:<20} {}'.format(network['ssid'], network['ip_address']))
