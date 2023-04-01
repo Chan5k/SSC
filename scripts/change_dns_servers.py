e94c355ad472442421f46037171f1863cd3e3319
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 19:01:39 +0300

    Update change_dns_servers.py

--- scripts/change_dns_servers.py
+++ scripts/change_dns_servers.py
@@ -1,31 +1,33 @@
-import os
-
+import subprocess
 
 def change_dns_servers():
-    """
-    Changes the DNS servers to Cloudflare or Google.
-    """
-    print("Choose the DNS server you want to use:")
-    print("1. Cloudflare (1.1.1.2, 1.0.0.2)")
+    print("Which DNS servers would you like to use?")
+    print("1. Cloudflare (1.1.1.1, 1.0.0.1)")
     print("2. Google (8.8.8.8, 8.8.4.4)")
     selection = input("> ")
+
     if selection == "1":
-        nameservers = ["nameserver 1.1.1.2", "nameserver 1.0.0.2"]
+        nameservers = ["1.1.1.1", "1.0.0.1"]
     elif selection == "2":
-        nameservers = ["nameserver 8.8.8.8", "nameserver 8.8.4.4"]
+        nameservers = ["8.8.8.8", "8.8.4.4"]
     else:
         print("Invalid selection.")
         return
 
-    # Change /etc/resolv.conf
-    with open("/etc/resolv.conf", "w") as f:
-        f.write("\n".join(nameservers))
+    # Remove old nameservers from /etc/resolv.conf
+    subprocess.run(["sudo", "sed", "-i", "/nameserver/d", "/etc/resolv.conf"])
 
-    # Change /etc/resolvconf/resolv.conf.d/head
-    with open("/etc/resolvconf/resolv.conf.d/head", "w") as f:
-        f.write("\n".join(nameservers))
+    # Remove old nameservers from /etc/resolvconf/resolv.conf.d/head
+    subprocess.run(["sudo", "sed", "-i", "/nameserver/d", "/etc/resolvconf/resolv.conf.d/head"])
 
-    # Update resolv.conf
-    os.system("resolvconf -u")
+    # Add new nameservers to /etc/resolv.conf and /etc/resolvconf/resolv.conf.d/head
+    with open("/etc/resolv.conf", "a") as f:
+        for nameserver in nameservers:
+            f.write("nameserver {}\n".format(nameserver))
+    with open("/etc/resolvconf/resolv.conf.d/head", "a") as f:
+        for nameserver in nameservers:
+            f.write("nameserver {}\n".format(nameserver))
 
+    # Restart the resolvconf service to apply the changes
+    subprocess.run(["sudo", "systemctl", "restart", "resolvconf"])
     print("DNS servers updated successfully.")
