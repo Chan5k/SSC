19269261f9d56f0df591d833b04bc78e2382ea76
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 18:35:57 +0300

    Update improve_internet_speed.py

--- scripts/improve_internet_speed.py
+++ scripts/improve_internet_speed.py
@@ -1,21 +1,50 @@
+import os
+import sys
 import subprocess
 
 def improve_internet_speed(interface):
-    # disable IPv6
-    subprocess.run(["sudo", "sysctl", "-w", f"net.ipv6.conf.{interface}.disable_ipv6=1"])
+    print("This will make changes to your system's network settings. Do you wish to continue? (y/n)")
+    confirmation = input("> ")
 
-    # enable TCP BBR congestion control
+    if confirmation.lower() != "y":
+        print("Aborting.")
+        sys.exit()
+
+    print("Improving internet speed...")
+
+    # Disable IPv6
+    subprocess.run(["sudo", "sysctl", "-w", "net.ipv6.conf.all.disable_ipv6=1"])
+    subprocess.run(["sudo", "sysctl", "-w", "net.ipv6.conf.default.disable_ipv6=1"])
+
+    # Enable TCP Fast Open
+    subprocess.run(["sudo", "sysctl", "-w", "net.ipv4.tcp_fastopen=3"])
+
+    # Enable TCP congestion control BBR
     subprocess.run(["sudo", "modprobe", "tcp_bbr"])
-    with open("/etc/sysctl.conf", "a") as file:
-        file.write("\n# Enable TCP BBR congestion control\n")
-        file.write("net.core.default_qdisc=fq\n")
-        file.write(f"net.ipv4.tcp_congestion_control=bbr\n")
+    subprocess.run(["sudo", "sysctl", "-w", "net.core.default_qdisc=fq"])
+    subprocess.run(["sudo", "sysctl", "-w", "net.ipv4.tcp_congestion_control=bbr"])
 
-    # set the MTU to 9000
-    subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "mtu", "9000"])
+    # Prompt the user to choose a DNS server
+    print("Which DNS server do you want to use? Enter the corresponding number:")
+    print("1. Google DNS (8.8.8.8, 8.8.4.4)")
+    print("2. Cloudflare DNS (1.1.1.1, 1.0.0.1)")
+    dns_selection = input("> ")
 
-    # restart the network interface
-    subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "down"])
-    subprocess.run(["sudo", "ip", "link", "set", "dev", interface, "up"])
-    
-    print("Internet speed improved!")
+    # Set DNS servers based on user's choice
+    if dns_selection == "1":
+        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "8.8.8.8", "--interface", interface])
+        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "8.8.4.4", "--interface", interface])
+        print("DNS servers set to Google DNS (8.8.8.8, 8.8.4.4)")
+    elif dns_selection == "2":
+        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "1.1.1.1", "--interface", interface])
+        subprocess.run(["sudo", "systemd-resolve", "--set-dns", "1.0.0.1", "--interface", interface])
+        print("DNS servers set to Cloudflare DNS (1.1.1.1, 1.0.0.1)")
+    else:
+        print("Invalid selection.")
+        sys.exit(1)
+
+    print("Done.")
+
+if __name__ == "__main__":
+    interface = input("Enter the name of your network interface: ")
+    improve_internet_speed(interface)
