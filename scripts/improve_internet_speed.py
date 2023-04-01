f365e7388386f57b509228b535328f6f189fa112
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 18:53:06 +0300

    Update improve_internet_speed.py

--- scripts/improve_internet_speed.py
+++ scripts/improve_internet_speed.py
@@ -1,16 +1,26 @@
 import subprocess
 
 def improve_internet_speed():
-    print("This feature will change your DNS servers to Cloudflare or Google DNS servers.")
-    print("Please choose an option:")
-    print("1. Cloudflare")
-    print("2. Google")
-    choice = input("> ")
-    if choice == "1":
-        subprocess.run(["sudo", "sed", "-i", "s/nameserver.*/nameserver 1.1.1.2\nnameserver 1.0.0.2/g", "/etc/resolv.conf"])
-        print("DNS servers updated to Cloudflare.")
-    elif choice == "2":
-        subprocess.run(["sudo", "sed", "-i", "s/nameserver.*/nameserver 8.8.8.8\nnameserver 8.8.4.4/g", "/etc/resolv.conf"])
-        print("DNS servers updated to Google.")
-    else:
-        print("Invalid selection.")
+    # Ask the user which DNS provider to use
+    dns_provider = input("Which DNS provider do you want to use? (1 for Cloudflare, 2 for Google): ")
+    while dns_provider not in ["1", "2"]:
+        dns_provider = input("Invalid input. Please enter 1 for Cloudflare or 2 for Google: ")
+
+    if dns_provider == "1":
+        # Use Cloudflare DNS
+        nameservers = ["1.1.1.2", "1.0.0.2"]
+    elif dns_provider == "2":
+        # Use Google DNS
+        nameservers = ["8.8.8.8", "8.8.4.4"]
+
+    # Create the new resolv.conf contents
+    new_resolv_conf = "\n".join(["nameserver " + ns for ns in nameservers])
+
+    # Write the new resolv.conf contents to a temporary file
+    with open("/tmp/new_resolv_conf", "w") as f:
+        f.write(new_resolv_conf)
+
+    # Replace the current resolv.conf with the new one
+    subprocess.run(["sudo", "mv", "/tmp/new_resolv_conf", "/etc/resolv.conf"])
+    
+    print("DNS servers have been updated to", ", ".join(nameservers))
