7be075776fdfdf238d4fd4fa34a55bf565daa832
Chan <87248999+Chan5k@users.noreply.github.com>
2023-04-01 17:50:19 +0300

    updated

--- scripts/security_test.py
+++ scripts/security_test.py
@@ -2,6 +2,6 @@ import subprocess
 
 def test_security():
     print("Running security test...")
-    cmd = "sudo nmap -sS -sV localhost"
+    cmd = "sudo nmap -sS -sV --script vuln localhost"
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     print(result.stdout)
